e1111c5dec2e6e401ac5abff8a4a5277c8ded796
yehezkieldio <yehezkieldio35@gmail.com>
2022-04-18 14:48:26 +0800

    refactor: minor stuff

--- web/resources/views/livewire/auth/login.blade.php
+++ web/resources/views/livewire/auth/login.blade.php
@@ -58,41 +58,45 @@
 
 <section class="section">
     <div class="container is-fullhd">
-        <div class="login">
-            <p class="logotype subtitle">
-                Disnaker Finance Recap
-            </p>
-            <h1 class="greeting title">
-                Silahkan untuk login!
-            </h1>
-            <hr>
-            <div class="form-container">
-                <form wire:submit.prevent="login">
-                    @csrf
-                    <div class="field">
-                        <label class="label">Username</label>
-                        <div class="control">
-                            <input type="text" wire:model='username' class="input" autocomplete="off">
-                            @error('username')
-                                <span class="tag">
-                                    {{ $message }}
-                                </span>
-                            @enderror
-                        </div>
+        <div class="columns">
+            <div class="column is-half">
+                <div class="login">
+                    <p class="logotype subtitle">
+                        Disnaker Finance Recap
+                    </p>
+                    <h1 class="greeting title">
+                        Silahkan untuk login!
+                    </h1>
+                    <hr>
+                    <div class="form-container">
+                        <form wire:submit.prevent="login">
+                            @csrf
+                            <div class="field">
+                                <label class="label">Username</label>
+                                <div class="control">
+                                    <input type="text" wire:model='username' class="input" autocomplete="off">
+                                    @error('username')
+                                        <span class="tag is-danger">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Password</label>
+                                <div class="control">
+                                    <input type="password" wire:model='password' class="input" autocomplete="off">
+                                    @error('password')
+                                        <span class="tag is-danger">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <button class="call-to-action call-to-action-primary" type="submit">Login</button>
+                        </form>
                     </div>
-                    <div class="field">
-                        <label class="label">Password</label>
-                        <div class="control">
-                            <input type="password" wire:model='password' class="input" autocomplete="off">
-                            @error('password')
-                                <span class="tag">
-                                    {{ $message }}
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <button class="call-to-action call-to-action-primary" type="submit">Login</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
