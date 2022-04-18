4f4872c5b30e02eff8afdceb349c5b5adfb6f53e
yehezkieldio <yehezkieldio35@gmail.com>
2022-04-18 09:28:51 +0800

    refactor: fuck you `filter_array` and php arrays

--- web/app/Http/Livewire/Admin/Dashboard.php
+++ web/app/Http/Livewire/Admin/Dashboard.php
@@ -10,9 +10,29 @@ class Dashboard extends Component
 {
     public function render()
     {
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $summaries = Summary::all();
+
+        function filter_array($array, $term) {
+            $matches = array();
+            foreach($array as $a){
+                if($a['id'] == $term)
+                    $matches[]=$a;
+            }
+            return $matches;
+        }
+        $sekretariat = filter_array($summaries, '1');
+        $penta = filter_array($summaries, '2');
+        $lattas = filter_array($summaries, '3');
+        $hi = filter_array($summaries, '4');
+
         return view('livewire.admin.dashboard', [
             'summary' => Summary::all(),
-            'user' => Auth::user()
+            'user' => Auth::user(),
+            'sekretariat' => $sekretariat,
+            'penta' => $penta,
+            'lattas' => $lattas,
+            'hi' => $hi
         ]);
     }
 }
