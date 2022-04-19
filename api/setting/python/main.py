2e9e184373668e03ee7b2d7f8cddd5ca50a56112
NotHydra <rizky.irswanda115@gmail.com>
2022-04-19 10:16:34 +0800

    feat: button disabled if overboard

--- api/setting/python/main.py
+++ api/setting/python/main.py
@@ -1,24 +1,25 @@
-import enum
 import PySimpleGUI as sg
 import json
 
 class Main():
     def main():
         data = json.load(open("./api/setting/json/setting.json")) #path
-        current_data = data[0]
-        current_data_detail = current_data.get("detail")[0]
+
+        data_count = 0
+        detail_count = 0
+        
+        current_data = data[data_count]
+        current_data_detail = current_data.get("detail")[detail_count]
 
         current_data_attribute = ["name", "start_range", "end_range"]
         current_data_detail_attribute = ["active_sheet", "start_range", "end_range", "attribute"]
-
-        detail_count = 0
 
         list_data = [
             division.get("name") for division in data
         ]
 
         layout = [
-            [sg.Listbox(list_data, default_values=data[0].get("name"), key="listbox", enable_events=True, size=(100, 4), no_scrollbar=True, select_mode="single")],
+            [sg.Listbox(list_data, default_values=data[data_count].get("name"), key="listbox", enable_events=True, size=(100, 4), no_scrollbar=True, select_mode="single")],
             
             [sg.Text("Division")],
             [sg.InputText(default_text=current_data.get("id"), size=(100, 1), key="data_id", enable_events=True, readonly=True)],
@@ -29,7 +30,7 @@ class Main():
             [[sg.InputText(default_text=current_data_detail.get(attribute), size=(100, 1), key=f"detail_{attribute}", enable_events=True)] for attribute in current_data_detail_attribute],
             
             [
-                sg.Button('Previous', key="previous_button", enable_events=True),
+                sg.Button('Previous', key="previous_button", enable_events=True, disabled = True),
                 sg.Button('Next', key="next_button", enable_events=True)
             ]
         ]
@@ -45,29 +46,37 @@ class Main():
                 pass
 
             elif(event == "previous_button" or event == "next_button"):
-                name = values["data_name"]
+                if(event == "previous_button" and detail_count != 0):
+                    detail_count -= 1
+
+                elif(event == "next_button" and detail_count != len(data[data_count].get("detail"))-1):
+                    detail_count += 1 
+
+                if(type(data[data_count].get("detail")[detail_count]) == dict):
+                    window["detail_id"].update(data[data_count].get("detail")[detail_count].get("id"))
+
+                    for attribute in current_data_detail_attribute:
+                        window[f"detail_{attribute}"].update(data[data_count].get("detail")[detail_count].get(attribute))
+
+
+                elif(type(data[data_count].get("detail")[detail_count]) != dict):
+                    window["detail_id"].update(detail_count + 1)
+
+                    for attribute in current_data_detail_attribute:
+                        window[f"detail_{attribute}"].update("null")
             
-                for i in range(len(data)):
-                    if(data[i].get("name") == name):
-                        print(detail_count)
-                        if(event == "previous_button" and detail_count != 0):
-                            detail_count -= 1
 
-                        elif(event == "next_button" and detail_count != len(data[i].get("detail"))-1):
-                            detail_count += 1 
+            if(detail_count == 0):
+                window["previous_button"].update(disabled = True)
 
-                        if(type(data[i].get("detail")[detail_count]) == dict):
-                            window["detail_id"].update(data[i].get("detail")[detail_count].get("id"))
+            if(detail_count != 0):
+                window["previous_button"].update(disabled = False)
 
-                            for attribute in current_data_detail_attribute:
-                                window[f"detail_{attribute}"].update(data[i].get("detail")[detail_count].get(attribute))
+            if(detail_count == len(data[data_count].get("detail"))-1):
+                window["next_button"].update(disabled = True)
 
-
-                        elif(type(data[i].get("detail")[detail_count]) != dict):
-                            window["detail_id"].update(detail_count + 1)
-
-                            for attribute in current_data_detail_attribute:
-                                window[f"detail_{attribute}"].update("null")
+            if(detail_count != len(data[data_count].get("detail"))-1):
+                    window["next_button"].update(disabled = False)
 
 
         window.close()
