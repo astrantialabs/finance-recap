2de224b85d57bb508cb098e51276a65d2ab7d4c4
NotHydra <rizky.irswanda115@gmail.com>
2022-05-07 16:33:35 +0800

    feat: new create pdf system

--- api/backend/python/main.py
+++ api/backend/python/main.py
@@ -5,6 +5,7 @@ import datetime
 from excel import Excel
 from pymongo import MongoClient
 from dotenv import dotenv_values
+from win32com import client
 
 class Database():
     def get_cluster(mongoDBURI):
@@ -70,74 +71,84 @@ class Utility():
 
 
 class File():
-    def create_excel(file_path, data):
-        for division_count, division in enumerate(data):
+    def create_file(file_path, data):
+        system_path = os.getcwd()
+
+        for division in data:
             current_datetime = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+
             excel_folder_path = f"excel/{division.get('name').lower()}"
             excel_file_path = F"{current_datetime}.xlsx"
-
             full_excel_file_path = f"{file_path}/{excel_folder_path}/{excel_file_path}"
+            system_excel_path = os.path.join(system_path, full_excel_file_path)
 
             pdf_folder_path = f"pdf/{division.get('name').lower()}"
-
-            full_pdf_folder_path = f"{file_path}/{pdf_folder_path}"
+            pdf_file_path = F"{current_datetime}.pdf"
+            full_pdf_folder_path = f"{file_path}/{pdf_folder_path}/{pdf_file_path}"
+            system_pdf_path = os.path.join(system_path, full_pdf_folder_path)
 
             os.makedirs(f"{file_path}/{excel_folder_path}", exist_ok=True)
             os.makedirs(f"{file_path}/{pdf_folder_path}", exist_ok=True)
-            Excel.create_file(full_excel_file_path)
 
-            wb_excel = Excel(full_excel_file_path, 1)
-            wb_excel.write_value_singular("A1", f"Divisi: {division.get('name')}")
-            wb_excel.write_value_multiple("A3", "C3", ["No.", "Sub Kegiatan", "Realisasi"])
-            wb_excel.write_value_multiple("c4", "D4", ["Fisik", "Keuangan"])
-
-            wb_excel.merge("A3", "A4")
-            wb_excel.merge("B3", "B4")
-            wb_excel.merge("C3", "D3")
-
-            row_end_range = 4 + len(division.get('activity'))
-            wb_excel.border_multiple("A3", [4, row_end_range], "all", style="thin")
-            wb_excel.alignment_multiple("A3", [4, row_end_range], horizontal="center", vertical="center")
-            wb_excel.alignment_multiple("B5", [2, row_end_range], horizontal="left", vertical="center", wrap=True)
-            wb_excel.font_singular("A1", size=12, bold=True)
-            wb_excel.font_multiple("A3", "D4", bold=True)
-
-            for activity_count, activity in enumerate(division.get('activity')):
-                activity_physical_value = None
-                if(type(activity.get('physical')) in (int, float)):
-                    activity_physical_value = f"{activity.get('physical')}%"
-
-                activity_finance_value = None
-                if(type(activity.get('finance')) in (int, float)):
-                    activity_finance_value = f"{activity.get('finance')}%"
-
-                activity_value = [
-                    activity_count + 1,
-                    activity.get("activity"),
-                    activity_physical_value,
-                    activity_finance_value
-                ]
-
-                wb_excel.write_value_multiple(
-                    [1, 5 + activity_count], [4, 5 + activity_count], activity_value)
+            File.create_excel(file_path, division, full_excel_file_path)
+            File.create_pdf(system_excel_path, system_pdf_path)
 
 
-            wb_excel.workbook_sheet.column_dimensions["A"].width = 5
-            wb_excel.workbook_sheet.column_dimensions["B"].width = 30
-            wb_excel.workbook_sheet.column_dimensions["C"].width = 11
-            wb_excel.workbook_sheet.column_dimensions["D"].width = 11
-            wb_excel.workbook.save(wb_excel.path)
+    def create_excel(file_path, division, full_excel_file_path):
+        Excel.create_file(full_excel_file_path)
+
+        wb_excel = Excel(full_excel_file_path, 1)
+        wb_excel.write_value_singular("A1", f"Divisi: {division.get('name')}")
+        wb_excel.write_value_multiple("A3", "C3", ["No.", "Sub Kegiatan", "Realisasi"])
+        wb_excel.write_value_multiple("c4", "D4", ["Fisik", "Keuangan"])
+
+        wb_excel.merge("A3", "A4")
+        wb_excel.merge("B3", "B4")
+        wb_excel.merge("C3", "D3")
+
+        row_end_range = 4 + len(division.get('activity'))
+        wb_excel.border_multiple("A3", [4, row_end_range], "all", style="thin")
+        wb_excel.alignment_multiple("A3", [4, row_end_range], horizontal="center", vertical="center")
+        wb_excel.alignment_multiple("B5", [2, row_end_range], horizontal="left", vertical="center", wrap=True)
+        wb_excel.font_singular("A1", size=12, bold=True)
+        wb_excel.font_multiple("A3", "D4", bold=True)
+
+        for activity_count, activity in enumerate(division.get('activity')):
+            activity_physical_value = None
+            if(type(activity.get('physical')) in (int, float)):
+                activity_physical_value = f"{activity.get('physical')}%"
+
+            activity_finance_value = None
+            if(type(activity.get('finance')) in (int, float)):
+                activity_finance_value = f"{activity.get('finance')}%"
+
+            activity_value = [
+                activity_count + 1,
+                activity.get("activity"),
+                activity_physical_value,
+                activity_finance_value
+            ]
+
+            wb_excel.write_value_multiple(
+                [1, 5 + activity_count], [4, 5 + activity_count], activity_value)
 
 
-    def create_pdf():
-        # task = OfficeToPdf('project_public_3aa50bd9a581100935a47732c8d97198_hK0jz4270624ccb2b3e6a215595cf22e7b6a9', verify_ssl=True, proxies=None)
-        # task.add_file(full_excel_file_path)
-        # task.set_output_folder(full_pdf_folder_path)
-        # task.execute()
-        # task.download()
-        # task.delete_current_task() 
+        wb_excel.workbook_sheet.column_dimensions["A"].width = 5
+        wb_excel.workbook_sheet.column_dimensions["B"].width = 30
+        wb_excel.workbook_sheet.column_dimensions["C"].width = 11
+        wb_excel.workbook_sheet.column_dimensions["D"].width = 11
+        wb_excel.workbook.save(wb_excel.path)
 
-        print()    
+
+    def create_pdf(system_excel_path, system_pdf_path):
+        excel = client.Dispatch("Excel.Application")
+
+        sheets = excel.Workbooks.Open(system_excel_path)
+        work_sheets = sheets.Worksheets[0]
+    
+        work_sheets.ExportAsFixedFormat(0, system_pdf_path)
+
+        excel.Application.Quit()
 
 
 class Main(Database, Utility, File):
@@ -172,14 +183,14 @@ class Main(Database, Utility, File):
         if(Main.env_value.get("Status") == "Production"):
             Main.write_json(division_array, Main.env_value.get("JSONPath"))
             data = json.load(open(Main.env_value.get("JSONPath")))
-            Main.update_data(mongoDBURI, database_name, data)
+            # Main.update_data(mongoDBURI, database_name, data)
 
-            Main.create_excel(Main.env_value.get("FilePath"), data)            
+            Main.create_file(Main.env_value.get("FilePath"), data)            
 
         elif(Main.env_value.get("Status") == "Non-Production"):
-            Main.update_data(mongoDBURI, database_name, division_array)
+            # Main.update_data(mongoDBURI, database_name, division_array)
 
-            Main.create_excel(Main.env_value.get("FilePath"), division_array)         
+            Main.create_file(Main.env_value.get("FilePath"), division_array)         
 
 
     def get_division(settings_data, wb_summary):
